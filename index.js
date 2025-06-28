de12c207e095df9c947655bb077a332cef265116
jayden-creat3 <jaydenkamau508@gmail.com>
2025-06-28 10:15:26 +0300

    sbmission

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert);
 }
+
+// Call the function to set up the event listener
+addingEventListener();
